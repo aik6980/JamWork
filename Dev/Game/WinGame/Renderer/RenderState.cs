4658e8bed0d237953fd31f8ca77cf7daf46cfa1e
aik6980 <aik_canhelp@hotmail.com>
2014-11-13 21:14:40 +0000

    add RenderState.cs

---
+++ Dev/Game/WinGame/Renderer/RenderState.cs
@@ -0,0 +1,37 @@
+﻿using SharpDX;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+
+namespace Renderer
+{
+    class RenderStateGlobal : Util.Singleton<RenderStateGlobal>
+    {
+        SamplerState m_BilinearClampSampler;
+        public SamplerState BilinearClampSampler
+        {
+            get { return m_BilinearClampSampler; }
+        }
+
+
+        public void Init()
+        {
+            var device = Renderer.RenderDevice.Instance().Device;
+
+            {
+                SamplerStateDescription description = SamplerStateDescription.Default();
+                description.Filter = Filter.MinMagMipLinear;
+                description.AddressU = TextureAddressMode.Wrap;
+                description.AddressV = TextureAddressMode.Wrap;
+                m_BilinearClampSampler = new SamplerState(device, description);
+            }
+        }
+
+        public void Destroy()
+        {
+            m_BilinearClampSampler.Dispose();
+        }
+    };
+}
